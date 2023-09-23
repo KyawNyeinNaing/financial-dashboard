7712f6e19ffdc24a81b388bd82891ed56e6dc732
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-09-23 11:09:24 +0700

    updated: sendApiRequest function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -28,11 +28,17 @@ export class FetchAPI {
   async sendApiRequest(
     api: string,
     parameters: ParameterType,
-    method: string = 'GET'
+    method: string = 'GET',
+    body?: {
+      name: string;
+      email: string;
+      phone: string;
+    }
   ): Promise<ApiResponse & RelativeFilm & RelativeHomeWorld> {
     const queryString = new URLSearchParams(parameters as any);
     return await fetch(`${this.baseUrl}/${api}?${queryString.toString()}`, {
       method,
+      body: JSON.stringify(body),
       next: {
         revalidate: 3600,
       },
@@ -52,90 +58,23 @@ export class FetchAPI {
       });
   }
 
-  async getPeoples(path: string, params: ParameterType): Promise<People> {
-    return await this.sendApiRequest(path, params);
+  async getApi(path: string, params: ParameterType): Promise<People> {
+    return await this.sendApiRequest(path, params, 'GET');
   }
 
-  async getRelativePeople(
+  async sendApi(path: string, params: ParameterType): Promise<People> {
+    return await this.sendApiRequest(path, params, 'POST');
+  }
+
+  async sendTextMessage(
     path: string,
-    people: string[],
-    params: ParameterType
-  ): Promise<{ data: PeopleResult[] }> {
-    try {
-      const data = await Promise.all(
-        people.map(async (each: any) => {
-          const match = each.match(/\/(\d+)\/$/);
-          const res = match && (await this.sendApiRequest(`${path}/${match[1]}`, params));
-          return res;
-        })
-      );
-      return { data };
-    } catch (error) {
-      return { data: [] };
+    params: ParameterType,
+    body: {
+      name: string;
+      email: string;
+      phone: string;
     }
-  }
-
-  async getRelativeHomeworld(
-    path: string,
-    homeworld: any,
-    params: ParameterType
-  ): Promise<RelativeHomeWorld> {
-    try {
-      const match = homeworld?.match(/\/(\d+)\/$/);
-      if (!match) return { status: false };
-      const res = match && (await this.sendApiRequest(`${path}/${match[1]}`, params));
-      return {
-        name: res?.name,
-        terrain: res?.terrain,
-        climate: res?.climate,
-        residents: res?.residents,
-      };
-    } catch (error) {
-      return {
-        status: false,
-      };
-    }
-  }
-
-  async getPlanets(path: string, params: ParameterType): Promise<Planet> {
-    return await this.sendApiRequest(path, params);
-  }
-
-  async getFilms(path: string, params: ParameterType): Promise<Film> {
-    return await this.sendApiRequest(path, params);
-  }
-
-  async getRelativeFilm(
-    path: string,
-    films: any,
-    params: ParameterType
-  ): Promise<{ data: RelativeFilm[] }> {
-    try {
-      const data = await Promise.all(
-        films.map(async (each: any) => {
-          const match = each.match(/\/(\d+)\/$/);
-          const res = match && (await this.sendApiRequest(`${path}/${match[1]}`, params));
-          return {
-            title: res?.title,
-            url: res?.url,
-          };
-        })
-      );
-      return { data };
-    } catch (error) {
-      return { data: [] };
-    }
-  }
-
-  async getVehicle(path: string, params: ParameterType): Promise<Vehicle> {
-    return await this.sendApiRequest(path, params);
-  }
-
-  async getStarShips(path: string, params: ParameterType): Promise<StarShips> {
-    return await this.sendApiRequest(path, params);
-  }
-
-  async getSpecies(path: string, params: ParameterType): Promise<Species> {
-    return await this.sendApiRequest(path, params);
+  ) {
+    return await this.sendApiRequest(path, params, 'POST', body);
   }
 }
